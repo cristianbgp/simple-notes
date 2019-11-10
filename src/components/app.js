909725c0b0acb701aa2f120df97f99209fe6daca
cristianbgp <cristian.granda.pastor@gmail.com>
2019-11-10 17:43:30 -0500

    Use css grid

--- src/components/app.js
+++ src/components/app.js
@@ -3,23 +3,29 @@ import styled from "styled-components";
 import useLocalStorage from "../hooks/use-local-storage";
 
 const Container = styled.div`
+  display: grid;
+  grid-template-areas: "header" "text";
+  grid-template-rows: 1fr 93%;
   height: 100%;
 `;
-const Title = styled.h1``;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 200;
+`;
 
 const Header = styled.header`
-  height: 8%;
-  margin-bottom: 2%;
+  grid-area: header;
   display: flex;
   justify-content: center;
   align-items: center;
   border-bottom: 1px solid #eaeaea;
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.12);
+  z-index: 1;
 `;
 
 const TextArea = styled.textarea`
-  width: 100%;
-  height: 90%;
+  grid-area: text;
   padding: 3em;
   resize: none;
   border: none;
